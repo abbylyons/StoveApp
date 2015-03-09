59920c1e650504f31fec6a31e7621c28515efb40
Zahra Mahmood <zahramahmood16@gmail.com>
2015-03-09 19:36:01 -0400

    line graph is vsible

--- app/src/main/java/com/example/abby/stovetemperatureapp/ViewTempData.java
+++ app/src/main/java/com/example/abby/stovetemperatureapp/ViewTempData.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineDataSet;
+
+
+import java.util.ArrayList;
+
 
 public class ViewTempData extends ActionBarActivity {
 
@@ -12,6 +20,42 @@ public class ViewTempData extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_temp_data);
+
+        LineChart chart = new LineChart(getApplicationContext());
+
+        ArrayList<Entry> valsComp1 = new ArrayList<Entry>();
+        ArrayList<Entry> valsComp2 = new ArrayList<Entry>();
+
+        Entry c1e1 = new Entry(100.000f, 0);
+        valsComp1.add(c1e1);
+        Entry c1e2 = new Entry(50.000f, 1);
+        valsComp1.add(c1e2);
+
+        Entry c2e1 = new Entry(120.000f, 0);
+        valsComp2.add(c2e1);
+        Entry c2e2 = new Entry(110.000f, 1);
+        valsComp2.add(c2e2);
+
+        LineDataSet setComp1 = new LineDataSet(valsComp1, "Company 1");
+        LineDataSet setComp2 = new LineDataSet(valsComp2, "Company 2");
+
+        ArrayList<LineDataSet> dataSets = new ArrayList<LineDataSet>();
+        dataSets.add(setComp1);
+        dataSets.add(setComp2);
+
+        ArrayList<String> xVals = new ArrayList<String>();
+        xVals.add("1.Q"); xVals.add("2.Q"); xVals.add("3.Q"); xVals.add("4.Q");
+
+        LineData data = new LineData(xVals, dataSets);
+        chart.setData(data);
+        chart.invalidate();
+
+        setContentView(chart);
+
+
+
+
+
     }
 
 
@@ -19,6 +63,9 @@ public class ViewTempData extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_view_temp_data, menu);
+
+
+
         return true;
     }
 
