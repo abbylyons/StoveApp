5ddf7e957f3c42ca8453b1d816cb3043c08e6f35
ABIGAIL LYONS <ABIGAILLYONS@COLLEGE.HARVARD.EDU>
2015-03-09 19:00:56 -0400

    Added new activities for different types of data

--- app/src/main/java/com/example/abby/stovetemperatureapp/ViewTempData.java
+++ app/src/main/java/com/example/abby/stovetemperatureapp/ViewTempData.java
@@ -4,23 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
 
 
-public class MainActivity extends ActionBarActivity {
+public class ViewTempData extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_view_temp_data);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_view_temp_data, menu);
         return true;
     }
 
@@ -37,20 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void toViewTempData (View view) {
-        Intent intent = new Intent(this, ViewTempData.class);
-        startActivityForResult(intent, 0);
-    }
-
-    public void toViewSmokeData (View view) {
-        Intent intent = new Intent(this, ViewSmokeData.class);
-        startActivityForResult(intent, 0);
-    }
-
-    public void toCollectData (View view) {
-        Intent intent = new Intent(this, CollectData.class);
-        startActivityForResult(intent, 0);
     }
 }
